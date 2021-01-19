22951528aec559daa4a808fab5f6f7bd33c7e7a0
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-01-19 22:46:03 +0100

    simple react app showing time

---
+++ todo-app/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import DateTime from './Datetime' ;
+
+
+
+
+function App() {
+  const element = (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  );
+
+  return (
+    <div className="App">
+    {element}
+    <DateTime/>
+    </div>
+
+  )
+}
+
+export default App;
+
+
+
+
