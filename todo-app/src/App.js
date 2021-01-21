01f3ace0d8673a301888629143c586afb7709525
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-01-21 21:48:04 +0100

    added useState, still figuring it out

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,16 +1,34 @@
-import React from "react";
-import Form from './Components/Form'
+import React, {useEffect, useState} from "react";
 
 
 function App() {
+  const [todo, setTodo] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(todo);
+  }
+ 
+
+  
   return (
     <div className="App">
       <header>
         <h1>Todos</h1>
       </header>
-        <Form/>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="todo">Add Todo</label>
+          <input type="text" id="todo" onChange={e => setTodo(e.target.value)} value={todo}/>
+          <button onClick>Submit</button>
+        </form>
+
+        <h2>
+          {todo}
+        </h2>
       </div>
   );
 }
+
 
 export default App;
