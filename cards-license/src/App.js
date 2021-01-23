a61a0d4ba635cc471363f8f43314bbd58107910c
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-01-23 17:13:13 +0100

    formatted app component

--- cards-license/src/App.js
+++ cards-license/src/App.js
@@ -1,14 +1,27 @@
-import './App.css';
-import License from './Components/License'
+import "./App.css";
+import License from "./Components/License";
 
 function App() {
   return (
     <div className="App">
-      
-      <License name="Liban Rage" title="Fullstack Developer" age={21} image="./graduated.png"/>
-      <License name="Kelly Smith" title="IOS Developer" age={27} image="./profile2.png"/>
-      <License name="James Kennedy" title="React Native Developer" age={25} image="./profile.png"/>
-      
+      <License
+        name="Liban Rage"
+        title="Fullstack Developer"
+        age={21}
+        image="./graduated.png"
+      />
+      <License
+        name="Kelly Smith"
+        title="IOS Developer"
+        age={27}
+        image="./profile2.png"
+      />
+      <License
+        name="James Kennedy"
+        title="React Native Developer"
+        age={25}
+        image="./profile.png"
+      />
     </div>
   );
 }
