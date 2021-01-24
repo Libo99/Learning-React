dcfd8b6e8b0a2c6d81e97ef2aae8215941936846
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-01-24 20:38:55 +0100

    Added lifecycle methods and removed constructor

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,30 +1,24 @@
 import React from "react";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+//   constructor(props) {
+//     super(props);
 
-    this.state = { lat: null, errorMessage: "" };
+//     this.state = { lat: null, errorMessage: "" };
+//   }
+    state = {lat: null, errorMessage: ''}
 
+  componentDidMount() {
+    console.log("component is mounted");
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
-  componentDidMount(){
-      console.log('component is mounted');
+  componentDidUpdate() {
+    console.log("component is updated");
   }
-
-  componentDidUpdate(){
-      console.log('component is updated');
-  }
-
-  
 
   render() {
     if (this.state.errorMessage && !this.state.lat) {
