9b6f8fbec6afc98c0808e19e08728a610e3edb5d
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-02-03 21:35:28 +0100

    Added todolist and css for todolist

--- todo-app/src/Components/TodoForm.js
+++ todo-app/src/Components/TodoForm.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../TodoForm.css";
+import TodoList from './TodoList';
 
 
 const TodoForm = () => {
+const [todo, setTodo] = useState('');
+const [todos, setTodos] = useState([]);
 
+const Addtodo = (e) => {
+e.preventDefault();
+setTodos([ ...todos,
+    {
+    id: todos.length,
+    text: todo,
+
+}])
+}
 
     return(
-        <div className="form-container">
-            <div className="form">
+        <div className="container">
+            <div className="form-container">
                 <h2>Add a todo to the list</h2>
                 <form>
-                    <label htmlFor="todo">Add Todo</label>
-                    <input type="text" name="todo" id="todo"/>
+                    <label htmlFor="todo">Todo</label>
+                    <input type="text" name="todo" id="todo" value={todo} onChange={e => setTodo(e.target.value)}/>
+                    <button onClick={Addtodo}>Add Todo</button>
                 </form>
             </div>
+            <TodoList Todos={todos}/>
         </div>
     )
 }
---
+++ todo-app/src/Components/TodoList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../TodoList.css'
+
+const TodoList = (props) => {
+
+
+
+const {Todos} = props;
+    return(
+        <div className="list">
+            <h2 className="list-header">List of todos</h2>
+            <ul>
+                {Todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
+            </ul>
+        </div>
+    )
+
+}
+
+export default TodoList;
