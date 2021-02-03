0554b53ef3c20ef90cdf2b65948e58f7c888fd03
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-02-03 20:18:47 +0100

    css completed for the form

--- todo-app/src/Components/TodoForm.js
+++ todo-app/src/Components/TodoForm.js
@@ -8,6 +8,7 @@ const TodoForm = () => {
     return(
         <div className="form-container">
             <div className="form">
+                <h2>Add a todo to the list</h2>
                 <form>
                     <label htmlFor="todo">Add Todo</label>
                     <input type="text" name="todo" id="todo"/>
