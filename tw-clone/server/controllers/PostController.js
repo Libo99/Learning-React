716436c6eb24ce8a3a9285889f356a020d78345d
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-02-04 20:19:23 +0100

    added model, controller, route

---
+++ tw-clone/server/controllers/PostController.js
@@ -0,0 +1,24 @@
+const Post = require('../Models/Post');
+
+
+exports.GetPost = (req, res) => {
+    Post.find()
+    .then((data) => res.json(data))
+    .catch((err) => res.status(404).json({ msg: err }));
+}
+
+exports.CreatePost = (req, res) => {
+    const post = new Post({
+        name: req.body.name.toString().trim(),
+        text: req.body.text.toString().trim(),
+        created: Date.now()
+    })
+    post.save()
+    .then(data => res.status(201).json({
+        message: data
+    }))
+    
+}
+
+
+
---
+++ tw-clone/server/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+const PostRoute = require('./Routes/PostRoute');
+require('dotenv').config()
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log('Connecteeeeeed');
+});
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan('common'));
+
+
+app.get('/', (req, res) => {
+res.json({
+    message: 'hej'
+})
+});
+app.use('/post', PostRoute);
+
+
+
+
+const port = process.env.PORT || 8205;
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`)
+})
+
+
+
+
