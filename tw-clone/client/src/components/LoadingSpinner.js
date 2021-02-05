7ffaf1854a2cb9e87fab6be08c386c0400e43405
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-02-05 20:47:35 +0100

    added a loading spinner

---
+++ tw-clone/client/src/components/LoadingSpinner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import loading from '../assets/loading-kopi.gif'
+
+
+const LoadingSpinner = () => {
+
+
+    return(
+        <div>
+            <img src={loading} alt="loading"/>
+        </div>
+    )
+}
+
+
+export default LoadingSpinner;
