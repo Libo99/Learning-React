be25393e1d679559980ba0aea461db7d4964b10c
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-02-15 21:57:28 +0100

    made the app component an arrow function

--- tw-clone/client/src/App.js
+++ tw-clone/client/src/App.js
@@ -1,14 +1,14 @@
-import Form from './components/Form';
+import React from "react";
+import Form from "./components/Form";
 import List from "./components/List";
 
-
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Form />
       <List />
     </div>
   );
-}
+};
 
 export default App;
