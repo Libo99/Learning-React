9099f8649564a4d97adc7f01eec2ee14603a14b7
Liban-m <53412720+Liban-m@users.noreply.github.com>
2021-02-17 10:39:20 +0100

    Revert "removed the server since I already use an API from another project"
    This reverts commit d2aea5e4e41a806facab3061335942aa3bad8148.

---
+++ tw-clone/client/src/components/LoadingSpinner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import loading from '../assets/loading-kopi.gif'
+import './Loading.css'
+
+
+const LoadingSpinner = () => {
+
+
+    return(
+        <div className="loading-spinner">
+            <img src={loading} alt="loading"/>
+        </div>
+    )
+}
+
+
+export default LoadingSpinner;
